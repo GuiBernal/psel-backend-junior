53cacfafdf6613a2597347bc386cbfd4b8ddf663
Guido Bernal <guido.bernal@cubos.io>
2022-08-09 20:50:09 -0300

    test: add transaction mock and tests

---
+++ tests/mock/transaction.ts
@@ -0,0 +1,13 @@
+import { faker } from "@faker-js/faker";
+
+import type { TransactionBase } from "../../src/types/transaction";
+
+export function mockTransactionCreation(transaction: Partial<TransactionBase> = {}): TransactionBase {
+  return {
+    value: Number(faker.finance.amount()),
+    description: faker.random.words(),
+    cvv: faker.finance.creditCardCVV(),
+    type: "debit",
+    ...transaction,
+  };
+}
---
+++ tests/transaction.test.ts
@@ -0,0 +1,155 @@
+import supertest from "supertest";
+import { v4 } from "uuid";
+import app from "../src/app";
+import { mockAccountCreation } from "./mock/account";
+import { mockCardCreation } from "./mock/card";
+import { mockPeopleCreation } from "./mock/people";
+import { mockTransactionCreation } from "./mock/transaction";
+
+describe("Transaction", () => {
+  const request = supertest(app);
+
+  describe("Create Transaction", () => {
+    it("should create transaction", async () => {
+      const { body: people } = await request.post("/people").send(mockPeopleCreation());
+      const { body: account } = await request.post(`/people/${people.id}/accounts`).send(mockAccountCreation());
+      const { body: card } = await request.post(`/accounts/${account.id}/cards`).send(mockCardCreation());
+      const response = await request
+        .post(`/accounts/${account.id}/transactions`)
+        .send(mockTransactionCreation({ cvv: card.cvv }));
+
+      expect(response.status).toBe(201);
+      expect(response.body).toBeTruthy();
+    });
+
+    it("should return error when cvv is invalid", async () => {
+      const { body: people } = await request.post("/people").send(mockPeopleCreation());
+      const { body: account } = await request.post(`/people/${people.id}/accounts`).send(mockAccountCreation());
+      const response = await request
+        .post(`/accounts/${account.id}/transactions`)
+        .send(mockTransactionCreation({ cvv: "1a2" }));
+
+      expect(response.status).toBe(400);
+    });
+
+    it("should return error when account is not found", async () => {
+      const response = await request
+        .post(`/accounts/${v4()}/transactions`)
+        .send(mockTransactionCreation({ cvv: "123" }));
+
+      expect(response.status).toBe(404);
+      expect(response.text).toStrictEqual("Conta Não Encontrada");
+    });
+
+    it("should return error when balance is not enough", async () => {
+      const { body: people } = await request.post("/people").send(mockPeopleCreation());
+      const { body: account } = await request.post(`/people/${people.id}/accounts`).send(mockAccountCreation());
+      const { body: card } = await request.post(`/accounts/${account.id}/cards`).send(mockCardCreation());
+      const response = await request
+        .post(`/accounts/${account.id}/transactions`)
+        .send(mockTransactionCreation({ cvv: card.cvv, value: -100 }));
+
+      expect(response.status).toBe(400);
+      expect(response.text).toStrictEqual("Saldo Insuficiente");
+    });
+
+    it("should return error when card is not found", async () => {
+      const { body: people } = await request.post("/people").send(mockPeopleCreation());
+      const { body: account } = await request.post(`/people/${people.id}/accounts`).send(mockAccountCreation());
+      const response = await request
+        .post(`/accounts/${account.id}/transactions`)
+        .send(mockTransactionCreation({ cvv: "123" }));
+
+      expect(response.status).toBe(404);
+      expect(response.text).toStrictEqual("Cartão Não Encontrado");
+    });
+  });
+
+  describe("Get Transaction", () => {
+    it("should get transactions", async () => {
+      const { body: people } = await request.post("/people").send(mockPeopleCreation());
+      const { body: account } = await request.post(`/people/${people.id}/accounts`).send(mockAccountCreation());
+      const { body: card } = await request.post(`/accounts/${account.id}/cards`).send(mockCardCreation());
+      await request.post(`/accounts/${account.id}/transactions`).send(mockTransactionCreation({ cvv: card.cvv }));
+      const response = await request.get(`/accounts/${account.id}/transactions`);
+
+      expect(response.status).toBe(200);
+      expect(response.body.transactions).toBeTruthy();
+    });
+
+    it("should return error when account is not found", async () => {
+      const response = await request.get(`/accounts/${v4()}/transactions`);
+
+      expect(response.status).toBe(404);
+      expect(response.text).toStrictEqual("Conta Não Encontrada");
+    });
+  });
+
+  describe("Revert Transaction", () => {
+    it("should revert transaction", async () => {
+      const { body: people } = await request.post("/people").send(mockPeopleCreation());
+      const { body: account } = await request.post(`/people/${people.id}/accounts`).send(mockAccountCreation());
+      const { body: card } = await request.post(`/accounts/${account.id}/cards`).send(mockCardCreation());
+      const { body: transaction } = await request
+        .post(`/accounts/${account.id}/transactions`)
+        .send(mockTransactionCreation({ cvv: card.cvv, type: "credit" }));
+      const response = await request.post(`/accounts/${account.id}/transactions/${transaction.id}/revert`);
+
+      expect(response.status).toBe(201);
+      expect(response.body).toBeTruthy();
+      expect(response.body.type).toStrictEqual("debit");
+    });
+
+    it("should return error when account is not found", async () => {
+      const { body: people } = await request.post("/people").send(mockPeopleCreation());
+      const { body: account } = await request.post(`/people/${people.id}/accounts`).send(mockAccountCreation());
+      const { body: card } = await request.post(`/accounts/${account.id}/cards`).send(mockCardCreation());
+      const { body: transaction } = await request
+        .post(`/accounts/${account.id}/transactions`)
+        .send(mockTransactionCreation({ cvv: card.cvv }));
+      const response = await request.post(`/accounts/${v4()}/transactions/${transaction.id}/revert`);
+
+      expect(response.status).toBe(404);
+      expect(response.text).toStrictEqual("Conta Não Encontrada");
+    });
+
+    it("should return error when transaction is not found", async () => {
+      const { body: people } = await request.post("/people").send(mockPeopleCreation());
+      const { body: account } = await request.post(`/people/${people.id}/accounts`).send(mockAccountCreation());
+      const response = await request.post(`/accounts/${account.id}/transactions/${v4()}/revert`);
+
+      expect(response.status).toBe(404);
+      expect(response.text).toStrictEqual("Transação Não Encontrada");
+    });
+
+    it("should return error when transaction is already reverted", async () => {
+      const { body: people } = await request.post("/people").send(mockPeopleCreation());
+      const { body: account } = await request.post(`/people/${people.id}/accounts`).send(mockAccountCreation());
+      const { body: card } = await request.post(`/accounts/${account.id}/cards`).send(mockCardCreation());
+      const { body: transaction } = await request
+        .post(`/accounts/${account.id}/transactions`)
+        .send(mockTransactionCreation({ cvv: card.cvv }));
+      await request.post(`/accounts/${account.id}/transactions/${transaction.id}/revert`);
+      const response = await request.post(`/accounts/${account.id}/transactions/${transaction.id}/revert`);
+
+      expect(response.status).toBe(409);
+      expect(response.text).toStrictEqual("Transação já Revertida");
+    });
+
+    it("should return error when balance is not enough to revert", async () => {
+      const { body: people } = await request.post("/people").send(mockPeopleCreation());
+      const { body: account } = await request.post(`/people/${people.id}/accounts`).send(mockAccountCreation());
+      const { body: card } = await request.post(`/accounts/${account.id}/cards`).send(mockCardCreation());
+      const { body: transaction } = await request
+        .post(`/accounts/${account.id}/transactions`)
+        .send(mockTransactionCreation({ cvv: card.cvv, value: 100 }));
+      await request
+        .post(`/accounts/${account.id}/transactions`)
+        .send(mockTransactionCreation({ cvv: card.cvv, value: -50 }));
+      const response = await request.post(`/accounts/${account.id}/transactions/${transaction.id}/revert`);
+
+      expect(response.status).toBe(400);
+      expect(response.text).toStrictEqual("Saldo Insuficiente Para Reversão");
+    });
+  });
+});
